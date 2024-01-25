255e14d044b218cf299a50dc7d0a6040b9accb98
alffajardo <faj.alf@gmail.com>
2024-01-25 18:00:10 -0500

    added code to generate matlbatch filename

--- pet2MNI.sh
+++ pet2MNI.sh
@@ -10,6 +10,7 @@ tracer=$2 # NAV or TAU
 session=$3 # ses-01 or ses-02
 
 
+module load VilleneuveLab
 
 # Path to the prevent-ad root directory 
 PAD_PET="/project/rrg-villens/dataset/PreventAD/pet/derivatives/vlpp_preproc_2022/Nov2023"
@@ -22,11 +23,11 @@ warp_name=${subject}_${tracer}_${session}_anat2tpl_Warp.nii.gz
 # define the correct pet file
 if [ "$tracer" == "TAU" ]; then
 
-    pet_name="${subject}_${tracer}_${session}_pet_time-4070_space-anat_ref-infcereg_suvr.nii.gz"
+    pet_name="${subject}_${tracer}_${session}_pet_time-4070_space-anat_ref-infcereg_suvr.nii"
 
 elif [ "$tracer" == "NAV" ]; then
 
-    pet_name="${subject}_${tracer}_${session}_pet_time-4070_space-anat_ref-cerebellumCortex_suvr.nii.gz"
+    pet_name="${subject}_${tracer}_${session}_pet_time-4070_space-anat_ref-cerebellumCortex_suvr.nii"
 fi
 
 echo $pet_name
@@ -44,10 +45,10 @@ cd $tmp_dir
 
 # find the path to PET 
 
-pet_path=$(find $PAD_PET -name "$pet_name")
+pet_path=$(find $PAD_PET -name "${pet_name}.gz")
 
 # find the warp file
-warp_path=$(find $PAD_PET -name "$warp_name")
+warp_path=$(find $PAD_PET -name "${warp_name}.gz")
 
 echo ++ PET: $pet_path
 echo ++ WARP: $warp_path
@@ -57,11 +58,46 @@ echo
 
 # copy and decompressed the files 
 
-cp $pet_path $warp_path -t  $PWD
+cp $pet_path $warp_path -t  $tmp_dir
 
 gunzip -f  *.nii.gz
 
+## create the matlab batch  
 
+echo "%% A matlab script to normalise to MNI space
+
+
+% define the path to the  nifti image
+
+pet_file = PET_PATH
+% define the path to the warp (transformation params ) nifti file
+
+warp_file = WARP_PATH
+
+% Inicialize SPM batch job
+spm('Defaults', 'PET');
+spm_jobman('initcfg');
+
+
+% Load the PET scan
+pet = spm_vol(pet_file);
+
+% Load the warp file
+warp = spm_vol(warp_file);
+
+% Commands to normalise the file
+matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {pet_file};
+matlabbatch{1}.spm.spatial.normalise.write.subj.def = {warp_file};
+matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -50; 78 76 85];
+matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [1 1 1];
+matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 1;
+matlabbatch{1}.spm.spatial.normalise.write.woptions.prefix = '2MNI_';
+
+
+% run the matlab
+spm_jobman('run', matlabbatch);" | \
+sed "s/PET_PATH/${tmp_dir}/${pet_name}/g" | \
+sed "s/WARP_PATH/${tmp_dir}/${warp_name}"
 
 
 exit 0 
